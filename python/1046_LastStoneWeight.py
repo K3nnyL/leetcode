3bef67c3dad994de58265af82f0b0886c0d87dc8
K3nnyL <kennyhim1212@gmail.com>
2022-07-04 04:30:37 +0900

    day5

---
+++ python/1046_LastStoneWeight.py
@@ -0,0 +1,8 @@
+class Solution:
+    def lastStoneWeight(self, stones):
+        
+if __name__ == '__main__':
+    # begin
+    s = Solution()
+    Answer = s.lastStoneWeight([2,7,4,1,8,1])
+    print(Answer)
